753fb366c9808dcd479adead28eb86c09cd563e1
Achintya Pasricha <achintya@Achintyas-MacBook-Air.local>
2024-07-03 13:42:22 +0530

    added vercel speed insights-react

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 
 import Hero from "@/components/hero-home";
 import MarqueeTextComp from "@/components/marqueeText";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 export default function Home() {
